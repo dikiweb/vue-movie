23b774f19bc363709e765603ed8854640e0a0c03
dikiweb <maxjc@naver.com>
2021-06-18 17:17:21 +0900

    Create Project

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer>
+    <Logo />
+    <a
+      href="https://www.nidc.kr"
+      target="_blank">
+      &copy;{{ new Date().getFullYear() }} DIKI
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from "~/components/Logo"
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  footer {
+    padding: 70px 0;
+    text-align: center;
+    opacity: .5;
+
+    .logo {
+      display: block;
+      margin-bottom: 4px;
+    }
+  }
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: "DIKI",
+    email: "danny.j.park@gmail.com",
+    homePage: "https://www.nidc.kr/",
+    phone: "+82-10-7722-0437",
+    comimage: "https://www.nidc.kr/skin/img/logo/logo.png",
+    image: "https://heropy.blog/css/images/logo.png"
+  })
+}
